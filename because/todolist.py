09cc5c6ea8854162c6ee31ce0f3fdedd7f434244
Snorpi <118291778+Snorpi@users.noreply.github.com>
2024-08-09 14:17:47 -0600

    Adding more
    Fixing the PA elective data, adding a 7+1 option because I don't want to modularize this just yet because idk if I'll ever use it again

---
+++ because/todolist.py
@@ -0,0 +1,21 @@
+home_dailies = [
+
+]
+
+home_weeklies = [
+
+]
+
+home_monthlies = [
+
+]
+
+
+## important updates
+
+def checkSilverPrice():
+
+def calculateCryptoWorth():
+    atom_worth =
+    doge_worth =
+
--- work/7plus.py
+++ work/7plus.py
@@ -26,8 +26,8 @@ def generate_course_html(csv_file, elective_file, elective_column="elective", ):
                 elective_info = elective_data.get(elective, {})
                 elective_duration = elective_info.get('elective_duration', 0)
                 elective_name = elective_info.get('elective_name')
-                banking_fee = int(elective_info.get('elective_duration')) * 1.50
-                course_url = f"""{row['Month']}-{row['Day']}-24-{elective_duration}-{row['elective']}-ceq"""
+                banking_fee = int(elective_info.get('elective_duration')) * 1.50 + 10.5
+                course_url = f"""{row['Month']}-{row['Day']}-24-7-{elective_duration}-{row['elective']}-ceq"""
                 html_content = f"""
                 <p><a title="Click to learn more about {row['instructor_fname']}!" href="https://mortgageeducators.com/instructors" target="_blank" rel="noopener noreferrer"><img style="display: block; margin-left: auto; margin-right: auto;" src="https://mortgageeducators.com/images/00TemplateImages/Product_Banner/{row['instructor_fname']}_Banner.png" alt="" /></a></p>
                 <p> </p>
@@ -35,9 +35,9 @@ def generate_course_html(csv_file, elective_file, elective_column="elective", ):
                 <p style="text-align: center;"><span style="font-size: 18pt;"><strong>Start Time: </strong>{row['start_time']} - {row['end_time']} {row['timezone']} Time</span></p>
                 <p style="text-align: center;"><span style="font-size: 18pt;">({row['PT']} PT / {row['MT']} MT / {row['CT']} CT / {row['ET']} ET)</span></p>
                 <p> </p>
-                <p style="text-align: center;"><span style="text-decoration: underline; font-size: 24pt;"><strong>2024 {elective_duration} Hour {elective_name} CE Webinar</strong></span></p>
+                <p style="text-align: center;"><span style="text-decoration: underline; font-size: 24pt;"><strong>2024 7 + {elective_duration} Hour {elective_name} CE Webinar</strong></span></p>
                 <p> </p>
-                <p style="text-align: center;"><span style="font-size: 14pt;">  This course fulfills the {elective_duration} Hour {elective_name} NMLS mortgage continuing education requirement for 2024 in a live webinar format. </span></p>
+                <p style="text-align: center;"><span style="font-size: 14pt;">  This course fulfills the 7 + {elective_duration} Hour {elective_name} NMLS mortgage continuing education requirement for 2024 in a live webinar format. </span></p>
                 <p> </p>
                 <p><a title="NMLS Webcam Requirement" href="https://nationwidelicensingsystem.org/courseprovider/Course%Provider%Resources/Functional%Specification%for%All%NMLS%Approved%Courses.pdf#page=27" target="_blank" rel="noopener noreferrer"><img style="display: block; margin-left: auto; margin-right: auto;" title="NMLS Webcam Requirement" src="https://mortgageeducators.com/images/00TemplateImages/webcam4.png" alt="Webcam Required, Per NMLS as of June 6th, 2022." width="652" height="129" /></a></p>
                 <p style="text-align: left;"><span style="text-decoration: underline; font-size: 14pt;"><strong>Course Content:</strong></span></p>
@@ -48,25 +48,36 @@ def generate_course_html(csv_file, elective_file, elective_column="elective", ):
 
                 html_content += generate_chapter_html(elective_info.get('chapters', []))
                 html_content += f"""
-                <p><span style="font-size: 12pt;"><strong>***This live webinar course includes only the {elective_duration} hour(s) of {elective_name} CE material. If you need additional state education, those can be found <a href="https://mortgageeducators.com/articles?id=751" target="_blank" rel="noopener noreferrer">HERE</a> in an online format***</strong></span></p>
+                <p><span style="text-decoration: underline; font-size: 12pt;"><strong>7 Hour SAFE CORE Continuing Education Course: NMLS #15898</strong></span></p>
+                <p><span style="font-size: 12pt;">Chapter 1: Top 10 Violations</span></p>
+                <p><span style="font-size: 12pt;">Chapter 2: Unfair, Deceptive, Abusive Acts and Practices (UDAAP)</span></p>
+                <p><span style="font-size: 12pt;">Chapter 3: Loan Originator Compensation (LO Comp)</span></p>
+                <p><span style="font-size: 12pt;">Chapter 4: Mortgage Fraud &amp; Money Laundering</span></p>
+                <p><span style="font-size: 12pt;">Chapter 5: Fair Lending</span></p>
+                <p><span style="font-size: 12pt;">Chapter 6: Renovation/Rehabilitation Mortgages: FHA 203(k)</span></p>
+                <p><span style="font-size: 12pt;">Chapter 7: Non-Qualified Mortgages: How-To</span></p>
+                <p style="margin-top: 9px; margin-bottom: 9px; border-width: initial; border-style: none; line-height: 17px; font-size: 15px; text-align: left;"> </p>
+                <p style="margin-top: 9px; margin-bottom: 9px; border-width: initial; border-style: none; line-height: 17px; font-size: 15px; text-align: left;"><span style="font-size: 16px;">End of course group assessment</span></p>
+                <p style="text-align: left;"> </p>`
+                <p><span style="font-size: 12pt;"><strong>***This live webinar course includes only the 7 + {elective_duration} hours of {elective_name} CE material. If you need additional state education, those can be found <a href="https://mortgageeducators.com/articles?id=751" target="_blank" rel="noopener noreferrer">HERE</a> in an online format***</strong></span></p>
                 <p style="text-align: left;"><span style="font-size: 12pt;">Includes the ${banking_fee}0 NMLS banking fee. </span></p>
                 <p style="text-align: left;"><span style="font-size: 12pt;">This course is in a live webinar format and requires the student to be active and pay attention the entire time. Control measures will be taken to ensure   
                  participation. </span></p>
                 <p style="text-align: left;"><span style="font-size: 12pt;">Questions: <a href="mailto:webinar@MortgageEducators.com">webinars@MortgageEducators.com</a></span></p>
                 <p style="text-align: left;"><span style="font-size: 12pt;">NMLS Provider #1400062</span></p> 
-                <!-- 2024 {elective_duration} Hour {elective_name} CE Webinar {row['course_date']}, 2024 {row['start_time']} {row['timezone']} -->
-                <!-- {row['Month']}.{row['Day']}.2024 {elective_duration}hr {row['elective']} CEQ -->
+                <!-- 2024 7 + {elective_duration} Hour {elective_name} CE Webinar {row['course_date']}, 2024 {row['start_time']} {row['timezone']} -->
+                <!-- {row['Month']}.{row['Day']}.2024 - 7 + {elective_duration} {row['elective']} CEQ -->
                 <!-- {course_url} -->
-                <!-- {row['Month']}/{row['Day']} - {elective_duration} Hour {elective_name} CE Webinar -->
+                <!-- {row['Month']}/{row['Day']} - 7 + {elective_duration} Hour {elective_name} CE Webinar -->
                 <!-- asana info -->
                 <!-- {elective_duration}Hr {row['elective']} 
                 <h2 style="text-align: center">
                 <a
-                title="{elective_duration} Hour {elective_name} CE Webinar - {row['course_date']}"
+                title="7 + {elective_duration} Hour {elective_name} CE Webinar - {row['course_date']}"
                 href="https://mortgageeducators.com/component/virtuemart/{course_url}-detail"
                 target="_blank"
                 rel="noopener noreferrer"
-                >{elective_duration} Hour {elective_name} CE Webinar - {row['course_date']}</a
+                >7 + {elective_duration} Hour {elective_name} CE Webinar - {row['course_date']}</a
                 >
                 </h2>
                 """
